66ecfe519ea54f25f39d5f1fd66b6c83c36541e6
Euab <euanmills26@gmail.com>
2017-12-10 11:45:09 +0000

    Made a new window

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow} = electron;
+
+let menuWindow;
+let gameWindow;
+
+app.on('ready', function() {
+    menuWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    menuWindow.loadURL(url.format({
+        pathname: path.join(__dirname, './html/menuWindow.html'),
+        protocol: "file:",
+        slashes: true
+    }));
+    menuWindow.on('closed', function() {
+        app.quit()
+    });
+});
+
+// Gonna start to transplant the game here. Main menu will move to the on ready event
+function createGameWindow() {
+    gameWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Le Game'
+    });
+    gameWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'gameWindow.html'),
+        protocol: 'file:',
+        slashes:true
+    }));
+}
