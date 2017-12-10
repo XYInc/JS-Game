6566a1eb14e83c568751d7b8772680765ed92651
Euab <euanmills26@gmail.com>
2017-12-10 12:34:43 +0000

    Fixing code style

--- src/main.js
+++ src/main.js
@@ -2,38 +2,37 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
 const {app, BrowserWindow} = electron;
 
 let menuWindow;
 let gameWindow;
 
-app.on('ready', function() {
+app.on("ready", function() {
     menuWindow = new BrowserWindow({
         width: 800,
         height: 600
     });
     menuWindow.loadURL(url.format({
-        pathname: path.join(__dirname, './html/menuWindow.html'),
+        pathname: path.join(__dirname, "./html/menuWindow.html"),
         protocol: "file:",
         slashes: true
     }));
-    menuWindow.on('closed', function() {
-        app.quit()
+    menuWindow.on("closed", function() {
+        app.quit();
     });
 });
 
-// Gonna start to transplant the game here. Main menu will move to the on ready event
 function createGameWindow() {
     gameWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        title: 'Le Game'
+        title: "Le Game"
     });
     gameWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'gameWindow.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "gameWindow.html"),
+        protocol: "file:",
         slashes:true
     }));
 }
