f4889645ecb53e8678864f868ac287ba8ca6e008
Euab <euanmills26@gmail.com>
2017-12-10 00:50:56 +0000

    Close app stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const electron = require("electron")
-const url = require("url")
-const path = require("path")
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 
 const {app, chromiumWindow} = electron;
 
 let gameWindow;
 
-app.on("ready", function() {
+app.on('ready', function() {
     gameWindow = new chromiumWindow({});
     gameWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'gameWindow.html'),
         protocol: "file:",
         slashes: true
     }));
+    gameWindow.on('closed', function() {
+        app.quit()
+    });
 });
